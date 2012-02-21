1eefaac1dda94de1a4d4d06d2b93a96e722565c8
Yehonatan Daniv <maggotfish@gmail.com>
2012-02-21 10:04:18 +0200

    Removed dependency on Routed

--- src/js/widgets/Pane.js
+++ src/js/widgets/Pane.js
@@ -2,13 +2,12 @@
 (function (factory) {
     if ( typeof define === 'function' && define.amd ) {
         define([
-                'uijet_dir/uijet',
-                'uijet_dir/widgets/Base',
-                'uijet_dir/mixins/Layered',
-                'uijet_dir/mixins/Routed'
-            ], function (uijet) {
-                return factory(uijet);
-            });
+            'uijet_dir/uijet',
+            'uijet_dir/widgets/Base',
+            'uijet_dir/mixins/Layered'
+        ], function (uijet) {
+            return factory(uijet);
+        });
     } else {
         factory(uijet);
     }
@@ -16,28 +15,6 @@
     uijet.Widget('Pane', {
         options : {
             type_class  : 'uijet_pane'
-        },
-        wake            : function (context, by_route) {
-            var args;
-            // if invoked by route or user set the `force_wake` option to `true` then do full wake
-            if ( by_route || this.options.force_wake ) {
-                // enable waking logic to run
-                this.awake = false;
-                // continue with original waking
-                return this._super(context, by_route);
-            }
-            // if invoked by container widget and not a route then do a dry wake
-            if ( this.awake ) return this._finally();
-            // make sure we allow the user to check the state before we set this.container_context
-            args = ['pre_wake'].concat(Array.prototype.slice.call(arguments));
-            this.notify.apply(this, args);
-            // it's not a specific call for this pane so it's the container's context
-            this.container_context = context;
-            this.bind()
-                .appear()
-                .awake = true;
-            this.notify('post_wake');
-            return this._finally();
         }
-    }, ['Layered', 'Routed']);
+    }, ['Layered']);
 }));
