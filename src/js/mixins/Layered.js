66aa7950fce22983121370339ce0e94eebdc95da
Yehonatan Daniv <maggotfish@gmail.com>
2012-03-10 11:02:15 +0200

    Added a setting of layered class in prepareElement

--- src/js/mixins/Layered.js
+++ src/js/mixins/Layered.js
@@ -9,12 +9,21 @@
     }
 }(function (uijet) {
     uijet.Mixin('Layered', {
-        sleep       : function () {
+        layered         : true,
+        prepareElement  : function () {
+            this._super();
+            // since this relies on CSS this mixin must be included in the right order in relation to other mixins
+            // to make sure that the proper element is used below and, for example. that `setCurrent` is called in
+            // the right order with `transit`.
+            (this.$wrapper || this.$element).addClass('layered');
+            return this;
+        },
+        sleep           : function () {
             this._super();
             (this.$wrapper || this.$element).removeClass('current');
             return this;
         },
-        appear      : function () {
+        appear          : function () {
             this.setCurrent()
                 ._super();
             return this;
@@ -24,7 +33,7 @@
         // @return: this
         //
         // Hooks to `uijet.switchCurrent` to make sure this widget is the top layer among its sibling widgets.
-        setCurrent  : function () {
+        setCurrent      : function () {
             uijet.switchCurrent(this);
             return this;
         }
