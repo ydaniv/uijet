a6ff3c2e509738992480519137ff55129ca54d3f
Yehonatan Daniv <maggotfish@gmail.com>
2012-05-30 10:43:57 +0300

    Changed getting assets with a method to a simple config option; Added docs

--- src/js/mixins/Preloaded.js
+++ src/js/mixins/Preloaded.js
@@ -1,4 +1,3 @@
-//TODO: add docs
 // ### AMD wrapper
 (function (factory) {
     if ( typeof define === 'function' && define.amd ) {
@@ -11,40 +10,63 @@
 }(function ($, uijet) {
     uijet.Mixin('Preloaded', {
         preloaded   : true,
+        // ### widget.preload
+        // @sign: preload([assets])  
+        // @return: promises_array
+        //
+        // Preloads assets, specified either by the `assets` argument or the `options.assets`.  
+        // `assets` should be an `Array` of URLs to the assets.  
+        // Returns an `Array` of promise objects that resolve once all assets finished loading,
+        // successfully or not.
         preload     : function (assets) {
             var dfrds = [],
                 that = this,
                 IMG_EXTENSIONS = 'jpg|jpeg|png|gif';
-            (assets || this.getAssets()).forEach(function (path) {
+            // loop over the assets
+            (assets || uijet.Utils.returnOf(this.options.assets, this) || []).forEach(function (path) {
                 var dfrd;
+                // check if this is an image file by looking at its extension
                 if ( ~ IMG_EXTENSIONS.search(RegExp(path.substring(path.lastIndexOf('.')), 'i')) ) {
+                    // it's an image so call `preloadIamge`
                     dfrd = that.preloadImage(path);
                 }
+                // push the promise to the stack
                 dfrds.push(dfrd || {});
             });
             return dfrds;
         },
+        // ### widget.preloadImage
+        // @sign: preloadImage(path)  
+        // @return: promise
+        //
+        // Takes a path `String` and returns a promise that resolves when the image,
+        // loaded with the given `path` has completed loading either successfully or not.
         preloadImage: function (path) {
             var img, dfrd = $.Deferred();
+            // if the `preload_img_el` option is set
             if ( this.options.preload_img_el ) {
+                // use the DOM's createElement to create the image
                 img = document.createElement('img');
             } else {
+                // otherwise create a new `Image` object
                 img = new Image();
             }
+            // set the `src`
             img.src = path;
+            // if this image is already cached it might be done already
             if ( img.complete ) {
+                // clear the memory and resolve the promise
                 img = null;
                 dfrd.resolve();
             } else {
+                // image is not in cache so set a resolving handler on its events
                 img.onload = img.onerror = function () {
+                    // clear the memory and resolve the promise
                     img = null;
                     dfrd.resolve();
                 };
             }
             return dfrd.promise();
-        },
-        getAssets   : function () {
-            return this.assets || [];
         }
     });
 }));
