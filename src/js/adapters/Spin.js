e153cdaa56f00573d49bdf11acca835e136373f3
Yehonatan Daniv <maggotfish@gmail.com>
2012-06-04 10:30:38 +0300

    Implemented Spin.js adapter

---
+++ src/js/adapters/Spin.js
@@ -0,0 +1,37 @@
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define(['uijet_dir/uijet', 'plugins/spin'], function (uijet) {
+            return factory(uijet);
+        });
+    } else {
+        factory(uijet);
+    }
+}(function (uijet) {
+    uijet.Adapter('Spin', {
+        setSpinner  : function (switch_on) {
+            var spinner_defaults = {
+                    lines       : 12,
+                    length      : 12,
+                    width       : 6,
+                    radius      : 16,
+                    color       : '#fff',
+                    speed       : 1,
+                    trail       : 100,
+                    shadow      : true,
+                    hwaccel     : true,
+                    className   : 'uijet_spin_spinner'
+                },
+                $el = this.$wrapper || this.$element;
+            if ( switch_on ) {
+                if ( this.spinner ) {
+                    this.spinner.spin($el[0]);
+                } else {
+                    this.spinner = new Spinner(uijet.Utils.extend(spinner_defaults, this.options.spinner_options || {})).spin($el[0]);
+                }
+            } else {
+                this.spinner && this.spinner.stop();
+            }
+            return this;
+        }
+    });
+}));
