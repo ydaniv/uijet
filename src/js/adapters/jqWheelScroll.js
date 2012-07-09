3403db31f9614307da12003e62d1bf3ed5d4d8a1
Yehonatan Daniv <maggotfish@gmail.com>
2012-07-09 09:35:17 +0300

    Fixed multiple mousewheel event binding; Fixed horizontal check in mousewheel handler; Changed use of jQuery's .position() with simple offset checks

--- src/js/adapters/jqWheelScroll.js
+++ src/js/adapters/jqWheelScroll.js
@@ -9,21 +9,21 @@
 }(function (uijet) {
     uijet.Adapter('jqWheelScroll', {
         setScrolling: function (switch_on) {
-            var that = this,
+            var that = this, el, is_horizontal,
                 jqS_ops = {};
             if ( switch_on ) {
-                if ( this.options.horizontal ) {
+                if ( is_horizontal = this.options.horizontal ) {
                     jqS_ops.horizontal = true;
                     jqS_ops.vertical = false;
                 }
-                that._wrap().$wrapper.scroller(jqS_ops);
-                that.$element.mousewheel(function (e, delta, dx, dy) {
-                    var pos = that.$element.position();
-                    that.scrollTo(that.horizontal ? (-pos.left - 37*dx) : (-pos.top - 37*dy));
+                this._wrap().$wrapper.scroller(jqS_ops);
+                el = this.$element[0];
+                this.$element.unmousewheel().mousewheel(function (e, delta, dx, dy) {
+                    that.scrollTo(is_horizontal ? (-el.offsetLeft - 37*dx) : (-el.offsetTop - 37*dy));
                 });
 
             } else {
-                that.$wrapper && this.$wrapper.scroller('destroy');
+                this.$wrapper && this.$wrapper.scroller('destroy');
             }
             return this;
         },
