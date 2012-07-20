44b3efaeb0232f7a8b015d11f9b135d8291a536e
Yehonatan Daniv <maggotfish@gmail.com>
2012-07-20 21:15:03 +0300

    Fixed error with unreferenced variable left in fetchTemplate

--- src/js/mixins/Templated.js
+++ src/js/mixins/Templated.js
@@ -126,7 +126,7 @@
                     // set state to `has_tempalte`
                     that.has_template = true;
                     // tell the user we're done
-                    this.notify('post_fetch_template', response);
+                    this.notify('post_fetch_template');
                     // resolve the entire fetching promise
                     dfrd.resolve();
                 });
