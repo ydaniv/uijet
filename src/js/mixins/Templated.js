ce397590acee41d2955092875ed7a23214d6d54b
Yehonatan Daniv <maggotfish@gmail.com>
2012-07-20 18:07:23 +0300

    Re-implemented fetchTemplate to support patrials fetching

--- src/js/mixins/Templated.js
+++ src/js/mixins/Templated.js
@@ -70,33 +70,70 @@
         },
         // ### widget.fetchTemplate
         // @sign: fetchTemplate([refresh])  
-        // @return: XHR_promise OR {}
+        // @return: promise OR this
         //
         // Gets the template from the server to be used for this widget via XHR.  
+        // If `partials` option is set, an `Object` mapping partial name to its file name, loop over it and fetch all the partials needed.  
+        // If those partials are stored deeper under the global templates' path use the `partials_dir` option
+        // to specify it.  
         // If `refresh` is truthy then force the widget to fetch the template again.
         fetchTemplate   : function (refresh) {
             // if we don't have the template cached or was asked to refresh it
             if ( ! this.has_template || refresh ) {
+                // create a promise for retrieving all templates
+                var dfrd = $.Deferred(),
+                    // a stack for all template GET requests
+                    requests = [],
+                    // an error callback handler
+                    failure = function (response) {
+                        // tell the user we failed
+                        this.notify.apply(this, ['fetchTemplate_error'].concat(uijet.Utils.toArray(arguments)));
+                        // fail the whole fetching process
+                        dfrd.reject();
+                    },
+                    partials = this.options.partials,
+                    partials_dir = this.options.partials_dir || '',
+                    that = this, p;
                 // if asked to refresh then invalidate cache
                 refresh && (this.has_template = false);
-                // return the promise from the XHR call
-                return $.ajax({
-                    url     : this.getTemplateUrl(),
-                    type    : 'get',
+                // request the template
+                requests.push(this.getTemplateUrl(), $.ajax({
                     context : this
                 }).done( function (response) {
                     // cache result
                     this.template = response;
-                    this.has_template = true;
+                }).fail(failure));
+                // if we need to fetch partial templates
+                if ( partials ) {
+                    // loop over them
+                    for ( p in partials ) (function (name, path) {
+                        // build the path to each partial
+                        var partial_path = uijet.options.TEMPLATES_PATH +
+                                           partials_dir +
+                                           path + "." +
+                                           uijet.options.TEMPLATES_EXTENSION;
+                        // request that partial
+                        requests.push($.ajax(partial_path, {
+                            context : that
+                        }).done(function (partial) {
+                            // when done cache it
+                            this.partials[name] = partial;
+                        })).fail(failure);
+                    }(p, partials[p]));
+                }
+                // when all requests are resolved
+                $.when.apply($, requests).then(function () {
+                    // set state to `has_tempalte`
+                    that.has_template = true;
                     // tell the user we're done
                     this.notify('post_fetch_template', response);
-                }).fail( function (response) {
-                    // tell the user we failed
-                    this.notify.apply(this, ['fetchTemplate_error'].concat(uijet.Utils.toArray(arguments)));
+                    // resolve the entire fetching promise
+                    dfrd.resolve();
                 });
+                return dfrd.promise();
             }
             // like a fulfilled promise
-            return {};
+            return this;
         },
         render              : function () {
             // generate the HTML
