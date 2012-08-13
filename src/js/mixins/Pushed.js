45789a71ac0301742f5c7531839a01987384ef68
Yehonatan Daniv <maggotfish@gmail.com>
2012-08-13 15:07:09 +0300

    Implemented Pushed mixins

---
+++ src/js/mixins/Pushed.js
@@ -0,0 +1,50 @@
+// ### AMD wrapper
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define(['uijet_dir/uijet'], function (uijet) {
+            return factory(uijet);
+        });
+    } else {
+        factory(uijet);
+    }
+}(function (uijet) {
+    uijet.Mixin('Pushed', {
+        pushed  : true,
+        init    : function () {
+            var push_config;
+            this._super();
+            push_config = this.options.push_config;
+            if ( push_config && push_config.open_on_init ) {
+                this.open();
+            }
+            return this;
+        },
+        wake    : function () {
+            var push_config = this.options.push_config;
+            if ( ! (push_config && push_config.dont_open) ) {
+                this.open();
+            }
+            return this._super.apply(this, arguments);
+        },
+        sleep   : function () {
+            var push_config = this.options.push_config;
+            if ( ! (push_config && push_config.keep_open) ) {
+                this.close();
+            }
+            return this._super();
+        },
+        destroy : function () {
+            this.close();
+            return this._super();
+        },
+        getPushUrl: function () {
+            return uijet.Utils.returnOf(this.options.push_config.url);
+        },
+        open    : function (restart) {
+            return this;
+        },
+        close   : function () {
+            return this;
+        }
+    });
+}));
