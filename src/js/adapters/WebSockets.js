bab0e481c7839eb8177a691b66d7b709f967fca3
Yehonatan Daniv <maggotfish@gmail.com>
2012-08-13 15:07:33 +0300

    Implemented WebSockets adapter for Pushed mixin

---
+++ src/js/adapters/WebSockets.js
@@ -0,0 +1,58 @@
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define(['uijet_dir/uijet'], function (uijet) {
+            return factory(uijet);
+        });
+    } else {
+        factory(uijet);
+    }
+}(function (uijet) {
+    uijet.Adapter('WebSockets', {
+        open            : function (restart) {
+            var push_config = this.options.push_config;
+            this.socket_promise = uijet.Promise();
+            if ( restart ) {
+                this.socket && this.socket.close();
+                delete this.socket;
+            }
+            if ( ! this.socket ) {
+                this.socket = new WebSocket(this.getPushUrl(), push_config && uijet.Utils.returnOf(push_config.protocols, this));
+                this.socket.onopen = this._openHandler.bind(this);
+                this.socket.onclose = this._closeHandler.bind(this);
+                this.socket.onmessage = this._messageHandler.bind(this);
+                this.socket.onerror = this._errorHandler.bind(this);
+            }
+            return this.socket_promise.promise();
+        },
+        close           : function () {
+            this.socket && this.socket.close();
+            delete this.socket;
+            return this;
+        },
+        send            : function (msg) {
+            if ( this.socket && this.socket.readyState === 1 ) {
+                if ( typeof msg != 'string' ) {
+                    msg = JSON.stringify(msg);
+                }
+                this.socket.send(msg);
+            } else {
+                uijet.when( this.open() )
+                    .then(this.send.bind(this));
+            }
+            return this;
+        },
+        _openHandler    : function (e) {
+            this.socket_promise.resolve(e);
+            this.notify(true, 'socket_opened', e);
+        },
+        _closeHandler   : function (e) {
+            this.notify(true, 'socket_closed', e);
+        },
+        _messageHandler : function (e) {
+            this.notify(true, 'socket_message', e);
+        },
+        _errorHandler   : function (e) {
+            this.notify(true, 'socket_error', e);
+        }
+    });
+}));
