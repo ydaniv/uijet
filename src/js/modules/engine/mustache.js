6ed353ab6a111bd88188da9f2a89f1b790336910
Yehonatan Daniv <maggotfish@gmail.com>
2012-09-25 13:39:22 +0200

    Fixed mustache engine module adapter

--- src/js/modules/engine/mustache.js
+++ src/js/modules/engine/mustache.js
@@ -1,20 +1,20 @@
 // ### AMD wrapper
 (function (root, factory) {
     if ( typeof define === 'function' && define.amd ) {
-        define(['uijet_dir/uijet', 'mustache', 'uijet_dir/widgets/Base'], function (uijet) {
-            return factory(uijet);
+        define(['uijet_dir/uijet', 'mustache', 'uijet_dir/widgets/Base'], function (uijet, Mustache) {
+            return factory(uijet, Mustache);
         });
     } else {
-        root.uijet_engine = factory(uijet);
+        root.uijet_engine = factory(uijet, root.Mustache);
     }
-}(this, function (uijet) {
+}(this, function (uijet, Mustache) {
     return function () {
         uijet.use({
             generate: function () {
                 return Mustache.to_html(this.template, this.data || this.context, this.partials);
             }
         }, uijet.BaseWidget.prototype);
-    
+
         return Mustache;
     };
 }));
