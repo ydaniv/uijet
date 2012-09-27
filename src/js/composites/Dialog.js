de8cc4a0ef4a9907af77bfbd97c0d616243ab7f2
Yehonatan Daniv <maggotfish@gmail.com>
2012-09-27 10:07:13 +0200

    Changed Dialog to Modal and created a new non-modal Dialog Composite

--- src/js/composites/Dialog.js
+++ src/js/composites/Dialog.js
@@ -4,8 +4,8 @@
         define([
             'uijet_dir/uijet',
             'uijet_dir/widgets/Base',
-            'uijet_dir/widgets/Overlay',
-            'uijet_dir/widgets/Button'
+            'uijet_dir/widgets/Button',
+            'uijet_dir/mixins/Floated'
 //            'uijet_dir/widgets/Pane'
         ], function (uijet) {
             return factory(uijet);
@@ -16,34 +16,13 @@
 }(function (uijet) {
     uijet.Widget('Dialog', {
         options : {
-            type_class: ['uijet_pane', 'uijet_modal'],
-            position  : 'center'
+            type_class: ['uijet_pane', 'uijet_dialog']
         },
         register: function () {
-            var overlay,
-                overlay_id,
-                buttons_configs,
+            var buttons_configs,
                 conf,
                 buttons;
             this._super();
-            // if this is a modal dialog
-            if ( this.options.modal ) {
-                // create the overlay element
-                overlay = document.createElement('div');
-                overlay_id = this.id + '_underlay';
-                overlay.id = overlay_id;
-                // append this as content element of the overlay
-                overlay.appendChild((this.$wrapper || this.$element)[0]);
-                // insert the overlay to the app's element's as its first child
-                uijet.$element[0].insertBefore(overlay, uijet.$element[0].firstChild);
-                // create the overlay widget
-                uijet.startWidget('Overlay', uijet.Utils.extend(true, {
-                    element     : overlay,
-                    id          : overlay_id,
-                    container   : this.id,
-                    darken      : true
-                }, this.options.underlay_options || {}), true);
-            }
             // if we have buttons to create
             if ( buttons_configs = uijet.Utils.toArray(this.options.buttons) ) {
                 buttons = [];
@@ -58,5 +37,5 @@
             }
             return this;
         }
-    });
+    }, ['Floated']);
 }));
---
+++ src/js/composites/Modal.js
@@ -0,0 +1,60 @@
+// ### AMD wrapper
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define([
+            'uijet_dir/uijet',
+            'uijet_dir/widgets/Base',
+            'uijet_dir/widgets/Overlay',
+            'uijet_dir/widgets/Button'
+//            'uijet_dir/widgets/Pane'
+        ], function (uijet) {
+            return factory(uijet);
+        });
+    } else {
+        factory(uijet);
+    }
+}(function (uijet) {
+    uijet.Widget('Modal', {
+        options : {
+            type_class: ['uijet_pane', 'uijet_modal'],
+            position  : 'center'
+        },
+        register: function () {
+            var overlay,
+                overlay_id,
+                buttons_configs,
+                conf,
+                buttons;
+            this._super();
+            // since this is a modal dialog,
+            // create the overlay element
+            overlay = document.createElement('div');
+            overlay_id = this.id + '_underlay';
+            overlay.id = overlay_id;
+            // append this as content element of the overlay
+            overlay.appendChild((this.$wrapper || this.$element)[0]);
+            // insert the overlay to the app's element's as its first child
+            uijet.$element[0].insertBefore(overlay, uijet.$element[0].firstChild);
+            // create the overlay widget
+            uijet.startWidget('Overlay', uijet.Utils.extend(true, {
+                element     : overlay,
+                id          : overlay_id,
+                container   : this.id,
+                darken      : true
+            }, this.options.underlay_options || {}), true);
+            // if we have buttons to create
+            if ( buttons_configs = uijet.Utils.toArray(this.options.buttons) ) {
+                buttons = [];
+                while ( conf = buttons_configs.shift() ) {
+                    conf.container || (conf.container = this.id);
+                    buttons.push({
+                        type    : 'Button',
+                        config  : conf
+                    });
+                }
+                uijet.startWidgets(buttons);
+            }
+            return this;
+        }
+    });
+}));
