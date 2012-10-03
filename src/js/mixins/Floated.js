76d60adf61f3d9f300e596779ac670bc6093f7ef
Yehonatan Daniv <maggotfish@gmail.com>
2012-10-03 12:43:32 +0200

    Moved the top position setting from prepareElement to render since the container might not get fully rendered by that stage and it won't position correct relatively

--- src/js/mixins/Floated.js
+++ src/js/mixins/Floated.js
@@ -21,19 +21,8 @@
         },
         floated         : true,
         prepareElement  : function () {
-            var top = uijet.Utils.returnOf(this.options.float_top, this),
-                bottom_style, rule;
             this._super()
                 .setFloat();
-            // check if float_top option was set
-            if ( top ) {
-                // find the last stylesheet in the document
-                bottom_style = document.styleSheets[document.styleSheets.length - 1];
-                // create a rule of positioning this floatee using the value of `float_top` option
-                rule = '#' + this.$wrapper.attr('id') + '.float.show { top: ' + top + 'px; }';
-                // insert this rule at that stylesheet's end
-                bottom_style.insertRule(rule, bottom_style.cssRules.length);
-            }
             return this;
         },
         // ### widget.setFloat
@@ -52,6 +41,26 @@
             // this enables custom widgets or mixins to predefine this method
             this._super && this._super();
             return this;
+        },
+        render          : function () {
+            // if top position wasn't set
+            if ( ! this._top_set ) {
+                // get the `float_top` option value if it is set
+                var top = uijet.Utils.returnOf(this.options.float_top, this),
+                    bottom_style, rule;
+                // check if `float_top` option was set
+                if ( top ) {
+                    // find the last stylesheet in the document
+                    bottom_style = document.styleSheets[document.styleSheets.length - 1];
+                    // create a rule of positioning this floatee using the value of `float_top` option
+                    rule = '#' + this.$wrapper.attr('id') + '.float.show { top: ' + top + 'px; }';
+                    // insert this rule at that stylesheet's end
+                    bottom_style.insertRule(rule, bottom_style.cssRules.length);
+                }
+            }
+            // make sure we set the top position only once
+            this._top_set = true;
+            return this._super();
         },
         appear          : function () {
             // needed to be set programmatically at the end to prevent Webkit from not setting the right height
