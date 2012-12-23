cee1b5d4557afacc0a2faa037f2381547752aff4
Yehonatan Daniv <maggotfish@gmail.com>
2012-12-23 13:17:29 +0200

    Replaced hard-coded 'click's with uijet.support.click_events.full; Refactored

--- src/widgets/Button.js
+++ src/widgets/Button.js
@@ -7,29 +7,31 @@
         factory(uijet);
     }
 }(function (uijet) {
+
+    var dom_events = {};
+    // by default bind this click/tap event handler
+    dom_events[uijet.support.click_events.full] = function (e) {
+        // publish the `pre_click` signal and allow user to disable `clicked` event
+        var _publish = this.notify(true, 'pre_click', e);
+        if ( _publish !== false ) {
+            // publish `clicked` event
+            this.publish('clicked', {
+                context : this.context,
+                event   : e
+            }).publish('app.clicked', {
+                id      : this.id,
+                event   : e
+            }, true);
+        }
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
     uijet.Widget('Button', {
         options         : {
             type_class  : 'uijet_button',
             dont_wrap   : true,
-            dom_events  : {
-                // by default bind this click event handler
-                click   : function (e) {
-                    // publish the `pre_click` signal and allow user to disable `clicked` event
-                    var _publish = this.notify(true, 'pre_click', e);
-                    if ( _publish !== false ) {
-                        // publish `clicked` event
-                        this.publish('clicked', {
-                            context : this.context,
-                            event   : e
-                        }).publish('app.clicked', {
-                            id      : this.id,
-                            event   : e
-                        }, true);
-                    }
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            }
+            dom_events  : dom_events
         },
         setInitOptions  : function () {
             var that = this, routing = that.options.routing;
@@ -37,13 +39,12 @@
             // if `data_url` option is set
             if ( this.options.data_url ) {
                 // bind another click handler 
-                this.$element.on('click', function () {
+                this.$element.on(uijet.support.click_events.full, function () {
                     // get `data_url` and run it as route, using `routing` option to determine whether it's inner
-                    that.runRoute(that.getDataUrl().path, typeof routing == 'undefined' ?
-                                                        true :
-                                                        typeof routing == 'function' ?
-                                                            ! routing.call(that, uijet.$(this)) :
-                                                            ! routing);
+                    that.runRoute(
+                        that.getDataUrl().path,
+                        typeof routing == 'undefined' ? true : ! uijet.returnOf(routing, that, uijet.$(this))
+                    );
                 });
             }
             return this;
