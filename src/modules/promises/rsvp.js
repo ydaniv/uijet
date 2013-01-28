15d1b4d4064cce279c3f26be011f8e770e68c801
Yehonatan Daniv <maggotfish@gmail.com>
2013-01-28 11:20:07 +0200

    Added adapter for RSVP.js as a Promises module

---
+++ src/modules/promises/rsvp.js
@@ -0,0 +1,45 @@
+(function (root, factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define(['uijet_dir/uijet', 'rsvp'], function (uijet) {
+            return factory(uijet, root.RSVP, root);
+        });
+    } else {
+        factory(uijet, root.RSVP, root);
+    }
+}(this, function (uijet, RSVP, root) {
+
+    var p_proto = RSVP.Promise.prototype;
+
+    if ( ! ('state' in p_proto) && ! ('promise' in p_proto) ) {
+        p_proto.state = function () {
+            var state = 'pending';
+            if ( this.isResolved ) {
+                state = 'resolved';
+            }
+            else if ( this.isRejected ) {
+                state = 'rejected';
+            }
+            return state;
+        };
+        p_proto.promise = function () {
+            return this;
+        };
+    }
+    else {
+        throw new Error('Integration Error: RSVP.Promise.prototype already has that property.');
+    }
+
+    uijet.use({
+        Promise     : function () {
+            return new RSVP.Promise();
+        },
+        when        : function () {
+            return root.all(arguments);
+        },
+        isPromise   : function (obj) {
+            return obj && uijet.Utils.isFunc(obj.then);
+        }
+    });
+
+    return RSVP;
+}));
