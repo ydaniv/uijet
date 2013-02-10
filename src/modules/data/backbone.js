882e5d97c2e259553570cd3b2054265fd512b1b2
Yehonatan Daniv <maggotfish@gmail.com>
2013-02-10 14:46:36 +0200

    Implemented data_events option

--- src/modules/data/backbone.js
+++ src/modules/data/backbone.js
@@ -17,11 +17,20 @@
     uijet.Base.extend(Backbone.Events);
 
     uijet.BaseWidget.prototype.register = function () {
-        var resource;
+        var resource, bindings, type, handler;
         baseRegister.call(this);
 
         if ( resource = this.options.resource ) {
             this.resource = new uijet.Resource(resource)();
+
+            if ( bindings = this.options.data_events ) {
+                for ( type in bindings ) {
+                    if ( typeof handler == 'string' ) {
+                        handler = this[handler];
+                    }
+                    this.listenTo(this.resource, type, handler);
+                }
+            }
         }
 
         return this;
