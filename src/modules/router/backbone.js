57a0c9352f83da973415fc289624aa2d1afd69f7
Yehonatan Daniv <maggotfish@gmail.com>
2013-04-04 08:45:40 +0300

    First implementation of a Backbone Router adapter

---
+++ src/modules/router/backbone.js
@@ -0,0 +1,66 @@
+(function (root, factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define([
+            'uijet_dir/uijet',
+            'backbone'
+        ], function (uijet, Backbone) {
+            return factory(uijet, Backbone);
+        });
+    } else {
+        uijet.router = factory(uijet, root.Backbone);
+    }
+}(this, function (uijet, Backbone) {
+
+    return function (router) {
+        var Router = router ? router : new Backbone.Router;
+
+        uijet.use({
+            setRoute    : function (widget, route, callback) {
+                var name;
+
+                if ( typeof callback == 'string' ) {
+                    name = callback;
+
+                    if ( callback in widget )
+                        callback =  widget[callback];
+
+                    else if ( callback in Router )
+                        return Router.route(route, name), this;
+
+                }
+
+                if ( typeof callback != 'function' ) {
+                    callback = widget.run;
+                }
+
+                Router.route(route, name, function () {
+                    var context = uijet.buildContext(route, arguments);
+                    callback.call(widget, context);
+                });
+
+                return this;
+            },
+            unsetRoute      : function (widget, route) {
+                var r;
+                for ( r in Router ) {
+                    if ( r === route ) {
+                        Router.off('route:' + Router[r]);
+                        delete Router[r];
+                    }
+                }
+                return this;
+            },
+            runRoute    : function (route) {
+                var r;
+                for ( r in Router ) {
+                    if ( r === route ) {
+                        Router.trigger('route:' + Router[r]);
+                        break;
+                    }
+                }
+                return this;
+            },
+            navigate    : Router.navigate.bind(Router)
+        });
+    };
+}));
