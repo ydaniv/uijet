16369aadff301abc192677be247c38ec5f6a352d
Yehonatan Daniv <maggotfish@gmail.com>
2013-08-06 11:56:15 +0300

    Bumped version to 0.0.19

--- src/uijet.js
+++ src/uijet.js
@@ -1,6 +1,6 @@
 /*!
  * UIjet UI Framework
- * @version 0.0.18
+ * @version 0.0.19
  * @license BSD License (c) copyright Yehonatan Daniv
  * https://raw.github.com/ydaniv/uijet/master/LICENSE
  */
@@ -699,7 +699,7 @@
      * @namespace uijet
      */
     uijet =  {
-        version             : '0.0.18',
+        version             : '0.0.19',
         route_prefix        : '',
         route_suffix        : '',
         init_queue          : [],
