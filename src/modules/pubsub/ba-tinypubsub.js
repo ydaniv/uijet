c934384c000c0c315f9573671c1f33dbd49fb557
Yehonatan Daniv <maggotfish@gmail.com>
2013-12-06 11:38:38 +0200

    Refactored handlers_cache into a separate helper module under modules/pubsub

--- src/modules/pubsub/ba-tinypubsub.js
+++ src/modules/pubsub/ba-tinypubsub.js
@@ -3,68 +3,17 @@
         define([
             'uijet_dir/uijet',
             'jquery',
+            'uijet_dir/modules/pubsub/_handlers-cache',
             'ba-tinypubsub'
-        ], function (uijet, $) {
-            return factory(root, uijet, $);
+        ], function (uijet, $, handlers_cache) {
+            return factory(root, uijet, $, handlers_cache);
         });
     } else {
-        factory(root, uijet, root.jQuery);
+        factory(root, root.uijet, root.jQuery, root.uijet_handlers_cache);
     }
-}(this, function (root, uijet, $) {
+}(this, function (root, uijet, $, handlers_cache) {
     // cache reference to Array's slice method
-    var arraySlice = root.Array.prototype.slice,
-        handlers_cache = {
-            handlers: [],
-            wrappers: [],
-            push    : function (handler, wrapper, topic) {
-                var index = this.handlers.indexOf(handler),
-                    wrapper_box;
-                if ( ~ index ) {
-                    this.wrappers[index][topic] = wrapper;
-                    this.wrappers[index].length++;
-                }
-                else {
-                    this.handlers.push(handler);
-                    wrapper_box = {};
-                    wrapper_box[topic] = wrapper;
-                    wrapper_box.length = 1;
-                    this.wrappers.push(wrapper_box);
-                }
-            },
-            pop     : function (topic, handler) {
-                var remove_indices = [],
-                    wrapper, index;
-                if ( handler ) {
-                    index = this.handlers.indexOf(handler);
-                    if ( ~ index ) {
-                        if ( wrapper = this.wrappers[index][topic] ) {
-                            delete this.wrappers[index][topic];
-                            this.wrappers[index].length--;
-                            if ( ! this.wrappers[index].length ) {
-                                this.wrappers.splice(index, 1);
-                                this.handlers.splice(index, 1);
-                            }
-                            return wrapper;
-                        }
-                    }
-                }
-                else {
-                    this.wrappers.forEach(function (box, n) {
-                        if ( box[topic] ) {
-                            delete box[topic];
-                            box.length--;
-                            if ( ! box.length ) {
-                                remove_indices.push(n);
-                            }
-                        }
-                    });
-                    while ( index = remove_indices.pop(), typeof index == 'number' ) {
-                        this.wrappers.splice(index, 1);
-                        this.handlers.splice(index, 1);
-                    }
-                }
-            }
-        };
+    var arraySlice = root.Array.prototype.slice;
 
     uijet.use({
         publish         : function (topic, data) {
