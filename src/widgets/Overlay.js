b1fcbb7a1ff81fa986875e4a9f9675a31ce809e6
Yehonatan Daniv <maggotfish@gmail.com>
2014-03-18 12:31:04 +0200

    Docs for Overlay widget

--- src/widgets/Overlay.js
+++ src/widgets/Overlay.js
@@ -1,5 +1,4 @@
 //TODO: try converting this widget into a mixin
-// ### AMD wrapper
 (function (factory) {
     if ( typeof define === 'function' && define.amd ) {
         define(['uijet_dir/uijet', 'uijet_dir/widgets/Base', 'uijet_dir/mixins/Layered'], function (uijet) {
@@ -9,16 +8,42 @@
         factory(uijet);
     }
 }(function (uijet) {
+
+    /**
+     * Overlay widget class.
+     * 
+     * @class Overlay
+     * @mixes uijet.BaseWidget
+     * @depends Layered
+     */
     uijet.Widget('Overlay', {
         options     : {
             type_class  : 'uijet_overlay'
         },
+        /**
+         * Invokes {@link Overlay#render} after init.
+         * 
+         * @memberOf Overlay
+         * @instance
+         * @param {Object} options - passed to `_super()`.
+         * @returns {Overlay}
+         */
         init        : function (options) {
             // complete `render` on `init`
             this._super(options).render()
                 ._finally();
             return this;
         },
+        /**
+         * Paints the container and makes sure its hidden.
+         * 
+         * Related options:
+         * * `darken`: adds the `darken` class to the top container element.
+         * 
+         * @memberOf Overlay
+         * @instance
+         * @returns {Overlay}
+         */
         render      : function () {
             this._super();
             if ( this.$wrapper ) {
@@ -33,6 +58,13 @@
             }
             return this;
         },
+        /**
+         * Puts the container on top (`z-index`) and make sure it's visible.
+         * 
+         * @memberOf Overlay
+         * @instance
+         * @returns {Overlay}
+         */
         appear      : function () {
             // make this top level by adding `z_top` class
             if ( this.$wrapper ) {
@@ -43,6 +75,14 @@
             this._super();
             return this;
         },
+        /**
+         * Moves the container from the top (`z-index`) and make sure it's hidden.
+         * 
+         * @memberOf Overlay
+         * @instance
+         * @param {boolean} [no_transitions] - pass to `_super()` call.
+         * @returns {Overlay}
+         */
         disappear   : function (no_transitions) {
             // remove `z_top` class
             if ( this.$wrapper ) {
