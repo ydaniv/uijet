d54225cfeba3110cf780c749c46202b627fb3a79
Yehonatan Daniv <maggotfish@gmail.com>
2014-03-25 14:22:30 +0200

    Added docs; Deprecated run(); Wrapped Backbone.Router.navigate() to return uijet

--- src/modules/router/backbone.js
+++ src/modules/router/backbone.js
@@ -11,10 +11,30 @@
     }
 }(this, function (uijet, Backbone) {
 
+    /**
+     * Router adapter for the Backbone.js router
+     * 
+     * @module modules/router/backbone
+     * @augments uijet
+     * @see {@link http://http://backbonejs.org/#Router|Backbone.Router}
+     * @param {Object} router - an instance of the Backbone.Router or a config object for its initialization.
+     */
     return function (router) {
         var Router = router instanceof Backbone.Router ? router : new Backbone.Router(router);
 
+        
         uijet.use({
+            /**
+             * Registers a route to be handled by a given `Routed` `widget` instance.
+             *
+             * @instance
+             * @param {Object} widget - the widget instance that will intercept this route.
+             * @param {string} [route] - the route to register to. Defaults to calling `widget.getRoute()`.
+             * @param {string|function} [callback] - key that maps to a method of the widget or the router, or a function,
+             * to use as callback for the route. Defaults to the `widget`'s `wake()`.
+             * @returns {uijet}
+             * @see {@link http://backbonejs.org/#Router|Backbone.Router#route}
+             */
             setRoute    : function (widget, route, callback) {
                 var name;
                 route = route || widget.getRoute();
@@ -31,7 +51,7 @@
                 }
 
                 if ( typeof callback != 'function' ) {
-                    callback = widget.run;
+                    callback = widget.wake;
                 }
 
                 Router.route(route, name, function () {
@@ -41,7 +61,15 @@
 
                 return this;
             },
-            unsetRoute  : function (widget, route) {
+            /**
+             * Removes a registered route.
+             * 
+             * @instance
+             * @param {string} route - a route that is registered on the router.
+             * @returns {uijet}
+             * @see {@link http://backbonejs.org/#Router|Backbone.Router#off}
+             */
+            unsetRoute  : function (route) {
                 var r;
                 for ( r in Router ) {
                     if ( r === route ) {
@@ -51,11 +79,29 @@
                 }
                 return this;
             },
+            /**
+             * Executes a handler for a given `route`.
+             * 
+             * @instance
+             * @param {string} route - the route to execute.
+             * @returns {uijet}
+             * @see {@link http://backbonejs.org/docs/backbone.html#section-195|Backbone.History#loadUrl}
+             */
             runRoute    : function (route) {
                 Backbone.history.loadUrl(route);
                 return this;
             },
-            navigate    : Router.navigate.bind(Router)
+            /**
+             * Delegates to the Backbone Router's `navigate()`.
+             * 
+             * @instance
+             * @returns {uijet}
+             * @see {@link http://http://backbonejs.org/#Router-navigate|Backbone.Router#navigate}
+             */
+            navigate    : function () {
+                Router.navigate.apply(Router, arguments);
+                return this;
+            }
         });
 
         return Router;
