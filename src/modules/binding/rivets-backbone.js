f959e53f8f496bd1a9ad9d91b77554416a07bf4b
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-16 12:34:55 +0300

    Fixed binding flow - don't skip binding if resource is set to an object and observe option is not set; Refactored data binding from register() into bindData(); Added dont_bind option to allow preventing auto binding on register and disable it for templated widgets by default

--- src/modules/binding/rivets-backbone.js
+++ src/modules/binding/rivets-backbone.js
@@ -1,23 +1,23 @@
-(function ( root, factory ) {
+(function (root, factory) {
     if ( typeof define === 'function' && define.amd ) {
         define([
             'uijet_dir/uijet',
             'rivets',
             'uijet_dir/widgets/Base'
-        ], function ( uijet, rivets ) {
+        ], function (uijet, rivets) {
             return factory(uijet, rivets);
         });
     }
     else {
         factory(uijet, root.rivets);
     }
-}(this, function ( uijet, rivets ) {
+}(this, function (uijet, rivets) {
 
     /*
      * Configure rivets to work with Backbone.js
      */
     rivets.adapters[':'] = {
-        subscribe  : function ( obj, keypath, callback ) {
+        subscribe  : function (obj, keypath, callback) {
             if ( obj instanceof Backbone.Collection ) {
                 obj.on('add remove reset sort', callback);
             }
@@ -25,7 +25,7 @@
                 obj.on('change:' + keypath, callback);
             }
         },
-        unsubscribe: function ( obj, keypath, callback ) {
+        unsubscribe: function (obj, keypath, callback) {
             if ( obj instanceof Backbone.Collection ) {
                 obj.off('add remove reset sort', callback);
             }
@@ -33,7 +33,7 @@
                 obj.off('change:' + keypath, callback);
             }
         },
-        read       : function ( obj, keypath ) {
+        read       : function (obj, keypath) {
             if ( obj instanceof Backbone.Collection ) {
                 return obj[keypath];
             }
@@ -41,7 +41,7 @@
                 return obj.get(keypath);
             }
         },
-        publish    : function ( obj, keypath, value ) {
+        publish    : function (obj, keypath, value) {
             if ( obj instanceof Backbone.Model ) {
                 obj.set(keypath, value);
             }
@@ -53,35 +53,9 @@
     base_widget_proto.register = function () {
         baseRegister.call(this);
 
-        var observables = uijet.utils.returnOf(this.options.observe, this),
-            k, observable;
-
-        if ( this.options.resource ) {
-
-            if ( ! observables ) {
-                observables = {};
-            }
-            if ( typeof this.options.resource == 'string' ) {
-                observables[this.options.resource] = this.resource || uijet.Resource(this.options.resource);
-            }
-            else {
-                // implicitly skip
-                return this;
-            }
+        if ( ! this.options.dont_bind && ! this.templated ) {
+            this.bindData();
         }
-
-        if ( observables ) {
-            for ( k in observables ) {
-                observable = observables[k];
-                if ( typeof observable == 'string' ) {
-                    observables[k] = uijet.Resource(observable);
-                }
-            }
-            observables[this.id] = this;
-
-            this.rv_view = rivets.bind(this.$wrapper || this.$element, observables, this.options.bind_options);
-        }
-
 
         return this;
     };
@@ -93,6 +67,39 @@
 
         return baseDestroy.apply(this, arguments);
     };
+
+    uijet.use({
+        bindData: function () {
+            var observables = uijet.utils.returnOf(this.options.observe, this),
+                k, observable;
+
+            if ( ! observables && this.options.resource ) {
+
+                observables = {};
+
+                if ( typeof this.options.resource == 'string' ) {
+                    observables[this.options.resource] = this.resource || uijet.Resource(this.options.resource);
+                }
+                else {
+                    // implicitly skip
+                    return this;
+                }
+            }
+
+            if ( observables ) {
+                for ( k in observables ) {
+                    observable = observables[k];
+                    if ( typeof observable == 'string' ) {
+                        observables[k] = uijet.Resource(observable);
+                    }
+                }
+                observables[this.id] = this;
+
+                this.rv_view = rivets.bind(this.$wrapper || this.$element, observables, this.options.bind_options);
+            }
+            return this;
+        }
+    }, base_widget_proto);
 
     return rivets;
 }));
