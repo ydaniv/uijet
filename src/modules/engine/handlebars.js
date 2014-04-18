13d7eb613ef6866a3b5c5844c5bcdbd96caed8fa
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-18 14:00:22 +0300

    Added docs

--- src/modules/engine/handlebars.js
+++ src/modules/engine/handlebars.js
@@ -7,10 +7,35 @@
         factory(uijet, root.Handlebars);
     }
 }(this, function (uijet, Handlebars) {
+    /**
+     * Handlebars engine module.
+     * 
+     * @module engine/handlebars
+     * @extends uijet
+     */
     uijet.use({
+        /**
+         * Renders a compiled template.
+         * 
+         * Supports `helpers` and `partials` in the second `options` argument,
+         * passed from `this.helpers` and `this.partials` respectively.
+         * 
+         * **note**: `this.partials` is set automatically in the `Templated` mixin.
+         * 
+         * @method module:engine/handlebars#generate
+         * @see {@link http://handlebarsjs.com/execution.html}
+         * @returns {string} - the rendered template.
+         */
         generate: function () {
             return this.template(this.getContext(), {helpers: this.helpers, partials: this.partials})
         },
+        /**
+         * Compiles a template string to an executable.
+         * 
+         * @method module:engine/handlebars#compile
+         * @param {string} template - template string to compile.
+         * @returns {function} - compiled template function.
+         */
         compile : function (template) {
             return Handlebars.compile(template);
         }
