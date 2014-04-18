d3e0e0c6690dd165fd981fd7528cb24029ec3097
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-18 15:47:21 +0300

    Added docs

--- src/modules/engine/lodash.js
+++ src/modules/engine/lodash.js
@@ -7,10 +7,36 @@
         factory(uijet, root._);
     }
 }(this, function (uijet, _) {
+    /**
+     * Lodash engine module.
+     * 
+     * @module engine/lodash
+     * @extends uijet
+     * @see {@link http://lodash.com/docs#template}
+     * @exports lodash
+     */
     uijet.use({
+        /**
+         * Renders a precompiled template.
+         * 
+         * @see {@link http://lodash.com/docs#template}
+         * @method module:engine/lodash#generate
+         * @returns {string} - the rendered template.
+         */
         generate: function () {
             return this.template(this.getContext());
         },
+        /**
+         * Precompiles a template string to an executable.
+         * 
+         * Related options:
+         * * `compile_options`: the `options` object argument sent
+         * 
+         * @see {@link http://lodash.com/docs#template}
+         * @method module:engine/lodash#compile
+         * @param {string} template - template string to compile.
+         * @returns {function} - precompiled template function.
+         */
         compile : function (template) {
             return _.template(template, null, this.options.compile_options);
         }
