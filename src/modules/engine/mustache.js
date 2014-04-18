d41bd2b67180c4c14e4c84e3237c7365c915a3b9
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-18 15:48:37 +0300

    Updated to support Mustache 0.8.x; Added docs

--- src/modules/engine/mustache.js
+++ src/modules/engine/mustache.js
@@ -7,12 +7,35 @@
         factory(uijet, root.Mustache);
     }
 }(this, function (uijet, Mustache) {
+    /**
+     * Mustache engine module.
+     * 
+     * @module engine/mustache
+     * @extends uijet
+     * @see {@link https://github.com/janl/mustache.js/#usage}
+     * @exports Mustache
+     */
     uijet.use({
+        /**
+         * Renders a template.
+         * 
+         * @see {@link https://github.com/janl/mustache.js/#usage}
+         * @method module:engine/mustache#generate
+         * @returns {string} - the rendered template.
+         */
         generate: function () {
-            return this.template(this.getContext(), this.partials);
+            return Mustache.render(this.template, this.getContext(), this.partials);
         },
+        /**
+         * Parses a template string ahead of time for faster rendering.
+         * 
+         * @see {@link https://github.com/janl/mustache.js/#pre-parsing-and-caching-templates}
+         * @method module:engine/mustache#compile
+         * @param {string} template - template string to compile.
+         * @returns {string} - the same `template` string.
+         */
         compile : function (template) {
-            return Mustache.compile(template);
+            return Mustache.parse(template), template;
         }
     }, uijet.BaseWidget.prototype);
 
