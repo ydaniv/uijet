c5f14817d8df63d46d196d5003b91c9609ce58eb
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-19 12:43:17 +0300

    Added docs; Added cast of returned value of isPromise() to a boolean

--- src/modules/promises/jquery.js
+++ src/modules/promises/jquery.js
@@ -1,4 +1,3 @@
-// ### AMD wrapper
 (function (root, factory) {
     if ( typeof define === 'function' && define.amd ) {
         define(['uijet_dir/uijet', 'jquery'], function (uijet, $) {
@@ -8,14 +7,54 @@
         factory(uijet, root.jQuery);
     }
 }(this, function (uijet, $) {
+    /**
+     * jQuery promises module.
+     * 
+     * @module promises/jquery
+     * @extends uijet
+     * @see {@link http://api.jquery.com/category/deferred-object/}
+     */
     uijet.use({
+        /**
+         * Constructs a deferred object.
+         * 
+         * @method module:promises/jquery#Promise
+         * @see {@link http://api.jquery.com/jQuery.Deferred/}
+         * @returns {Deferred} - a Deferred object.
+         */
         Promise     : $.Deferred,
-        whenAll     : function (dfrds) {
-            return $.when.apply($, dfrds);
+        /**
+         * Returns a Promise that is resolved once all
+         * Promises in the `promises` list are resolved,
+         * or rejected if one of those is rejected.
+         * 
+         * @method module:promises/jquery#whenAll
+         * @see {@link http://api.jquery.com/jQuery.when/}
+         * @param {*[]} promises - array of Promises and/or values.
+         * @returns {Promise}
+         */
+        whenAll     : function (promises) {
+            return $.when.apply($, promises);
         },
+        /**
+         * Converts any given argument into a Promise.
+         * If that argument is a Promise it returns it.
+         * 
+         * @method module:promises/jquery#when
+         * @see {@link http://api.jquery.com/jQuery.when/}
+         * @param {*} value - value or promise to convert into a thenable.
+         * @returns {Promise}
+         */
         when        : $.when,
+        /**
+         * Whether the given `obj` argument is a Promise.
+         * 
+         * @method module:promises/jquery#isPromise
+         * @param {*} obj - argument to check.
+         * @returns {boolean}
+         */
         isPromise   : function (obj) {
-            return obj && uijet.utils.isFunc(obj.then);
+            return !!(obj && uijet.utils.isFunc(obj.then));
         }
     }, uijet, $);
 }));
