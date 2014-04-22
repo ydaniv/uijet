d0ccfdbdef34e95e9b26116013b0a0ba92cc4881
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-22 12:12:40 +0300

    Added Docker for docs generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,8 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg    : grunt.file.readJSON('package.json'),
         clean  : {
-            docs: ['docs']
+            docs: ['docs'],
+            doks: ['doks']
         },
         connect: {
             docs: {
@@ -12,30 +13,46 @@ module.exports = function (grunt) {
                     base     : 'docs',
                     keepalive: true
                 }
+            },
+            doks: {
+                options: {
+                    port     : 8000,
+                    base     : 'doks',
+                    keepalive: true
+                }
             }
         },
         jsdoc  : {
             dist: {
-                src    : [
-                    'src/uijet.js',
-                    'src/widgets/',
-                    'src/mixins/',
-                    'src/modules/'
-                ],
+                src    : ['src/'],
                 jsdoc  : 'node_modules/.bin/jsdoc',
                 options: {
                     configure  : './conf.json',
                     destination: 'docs'
                 }
             }
+        },
+        docker : {
+            dist: {
+                src : ['src/**/*.js'],
+                dest: 'doks'
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
+    /* JSDoc based docs */
+    grunt.loadNpmTasks('grunt-jsdoc');
 
-    grunt.registerTask('docs', ['clean:docs', 'jsdoc:dist']);
+    grunt.registerTask('docs', ['clean:docs', 'jsdoc']);
     grunt.registerTask('rtfm', ['docs', 'connect:docs']);
+
+    /* Docker based docs */
+    grunt.loadNpmTasks('grunt-docker');
+
+    grunt.registerTask('doks', ['clean:doks', 'docker']);
+    grunt.registerTask('rtfd', ['doks', 'connect:doks']);
+
 };
