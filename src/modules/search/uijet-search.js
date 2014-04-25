545fb9aa9d12bbdcbc9b259ab27306d2786ddb3e
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-26 00:46:54 +0300

    Added docs

--- src/modules/search/uijet-search.js
+++ src/modules/search/uijet-search.js
@@ -11,30 +11,68 @@
     }
 }(this, function (uijet) {
 
+    /**
+     * Constructor for the search index class.
+     * 
+     * #### Available options:
+     * 
+     * * `fields` {@type Object}: a map of field names of the documents to search against.
+     * Currently values are ignored.
+     * * `ref` {@type string}: a field in the searched documents to use as reference for results.
+     * Defaults to `null` which uses the doc itself as ref, instead of a value of its field.
+     * * `query_flags` {@type string}: `RegExp`'s flags to use when performing search. Defaults to `'i'`.
+     * {@link https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions|See "Advanced Searching With Flags"}
+     * * `clean_term` {@type function}: method for cleaning the raw input term before searching.
+     * 
+     * @param {Object} [options] - instance configuration object.
+     * @returns {SearchIndex}
+     * @constructor
+     * @class SearchIndex
+     */
     function SearchIndex (options) {
-        if ( ! (this instanceof  SearchIndex) ) return new SearchIndex(options);
+        if ( ! (this instanceof SearchIndex) ) return new SearchIndex(options);
         options = options || {};
         this.fields = options.fields;
-        this.ref = options.ref || 'id';
+        this.ref = options.ref || null;
         this.query_flags = options.query_flags || 'i';
         this.clean_term = options.clean_term || null;
         this.documents = [];
     }
 
-    SearchIndex.prototype = {
+    SearchIndex.prototype = /** @lends SearchIndex.prototype */ {
         constructor : SearchIndex,
+        /**
+         * Sets documents to populate the index.
+         * 
+         * @param {Array} docs - documents to search in.
+         * @returns {SearchIndex}
+         */
         set         : function (docs) {
             this.documents = docs;
             return this;
         },
-        add         : function () {
-            this.documents = this.documents.concat.apply(this.documents, arguments);
+        /**
+         * Adds more documents to the index to search in.
+         * 
+         * @param {Array} docs - documents to add to the index.
+         * @returns {SearchIndex}
+         */
+        add         : function (docs) {
+            this.documents.push.apply(this.documents, docs);
             return this;
         },
-        search      : function (term, as_docs) {
+        /**
+         * Performs search using given `term` and returns list of results.
+         * 
+         * @param {string} term - the search term to use for searching the documents.
+         * @param {string|null} [ref] - override the `ref` option just for this search.
+         * @returns {Array} - results list. Can be a list of documents or values specified by `ref` option.
+         */
+        search      : function (term, ref) {
             var results = [],
                 fields = this.fields,
                 d = 0,
+                _ref = arguments.length > 1 ? ref : this.ref,
                 re, doc, field;
             if ( this.clean_term ) {
                 term = this.clean_term(term);
@@ -47,7 +85,7 @@
             for ( ; doc = this.documents[d]; d++ ) {
                 for ( field in fields ) {
                     if ( field in doc && re.test(doc[field]) ) {
-                        results.push(as_docs ? doc : doc[this.ref]);
+                        results.push(_ref ? doc[_ref] : doc);
                         break;
                     }
                 }
@@ -56,20 +94,73 @@
         }
     };
 
+    /**
+     * uijet-search search module.
+     * 
+     * @module search/uijet-search
+     * @extends uijet
+     */
     uijet.use({
+        /**
+         * @namespace search
+         * @memberOf module:search/uijet-search
+         */
         search  : {
+            /**
+             * @constructor
+             * @memberOf module:search/uijet-search.search
+             */
             Index   : SearchIndex
         }
     })
 
-    .use({
+    .use(
+    /**
+     * Mixin that adds searching logic to widgets.
+     * 
+     * @class Searched
+     * @lends Searched.prototype
+     * @extends uijet.BaseWidget
+     */
+    {
+        /**
+         * Constructs and initializes a search index.
+         * 
+         * #### Related options:
+         * 
+         * * `search` {@type Object}: {@link SearchIndex} instance configuration object.
+         * 
+         * @memberOf Searched
+         * @instance
+         * @param {Object} [options] - {@link SearchIndex} instance configuration object.
+         * Defaults to `search` config option.
+         * @returns {Widget}
+         */
         index       : function (options) {
             this.search_index = new uijet.search.Index(options || this.options.search);
             return this;
         },
-        search      : function () {
+        /**
+         * Performs search using given `term` and returns list of results.
+         * 
+         * @memberOf Searched
+         * @instance
+         * @param {string} term - the search term to use for searching the documents.
+         * @param {string|null} [ref] - override the `ref` option just for this search.
+         * @returns {Array} - results list. Can be a list of documents or values specified by `ref` option.
+         */
+        search      : function (term, ref) {
             return this.search_index.search.apply(this.search_index, arguments);
         },
+        /**
+         * Highlights results in given `text` by mathcing against `term`.
+         * 
+         * @memberOf Searched
+         * @instance
+         * @param {string} text - the text to search in.
+         * @param {string} term - the term to find and highlight.
+         * @returns {string} - the highlighted text.
+         */
         highlight   : function (text, term) {
             var re;
             try {
