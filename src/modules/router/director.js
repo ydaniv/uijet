56ad225bfe3aef7448b08f5b4cd9751f57699a09
Yehonatan Daniv <maggotfish@gmail.com>
2014-04-25 11:30:39 +0300

    Fixed initialization; Added optional config param to use for Router.configure(); Fixed setRoute() to use _parseHandler() for string callbacks or default to wake(); Added docs

--- src/modules/router/director.js
+++ src/modules/router/director.js
@@ -7,44 +7,79 @@
         uijet.router = factory(uijet, root);
     }
 }(this, function (uijet, root) {
-    return function (router) {
-        var Router;
-        if ( router ) {
-            Router = router;
+    /**
+     * Router adapter for the Director router.
+     * 
+     * @module module:router/director
+     * @extends uijet
+     * @see {@link https://github.com/flatiron/director|Director}
+     * @param {Object} router - an instance of `Router` or a config object for its initialization.
+     * @param {Object} [config] - configuration object for {@link https://github.com/flatiron/director#configuration|`Router.configure`}.
+     * @exports module:router/director
+     */
+    return function (router, config) {
+        var _router = router instanceof root.Router ? router : new root.Router(router);
+
+        if ( config ) {
+            _router.configure(config).init();
         }
-        else {
-            Router = root.Router().init();
+        else if ( ! _router.handler ) {
+            _router.init();
         }
+
         uijet.use({
+            /**
+             * Registers a route to be handled by a given `Routed` `widget` instance.
+             *
+             * @method module:router/director#setRoute
+             * @param {Object} widget - the widget instance that will intercept this route.
+             * @param {string} [route] - the route to register to. Defaults to calling `widget.getRoute()`.
+             * @param {string|function} [callback] - key that can be parsed to a handler, widget method,
+             * signal or event to publish, or a function, to use as callback for the route. Defaults to the `widget`'s `wake()`.
+             * @returns {uijet}
+             * @see {@link https://github.com/flatiron/director#adhoc-routing|Adhoc Routing}
+             */
             setRoute        : function (widget, route, callback) {
-                var method = 'get';
                 if ( uijet.utils.isObj(route) ) {
-                    method = route.method;
                     route = route.path;
                 }
-                if ( typeof callback == 'string' && widget[callback] ) {
-                    callback =  widget[callback];
-                }
+
+                callback =  widget._parseHandler(callback);
+
                 if ( typeof callback != 'function' ) {
-                    callback = widget.run
+                    callback = widget.wake;
                 }
+
                 route = route || widget.getRoute();
-                Router.on(route, function () {
+
+                _router.on(route, function () {
                     var context = uijet.buildContext(route, arguments);
                     callback.call(widget, context);
                 });
+
                 return this;
             },
             unsetRoute      : function (widget, route) {
+                //TODO: make a PR to flatiron/director with this feature
                 //TODO: TBD
                 return this;
             },
+            /**
+             * Executes a handler for a given `route`.
+             * 
+             * @method module:router/director#runRoute
+             * @param {string} route - the route to match for a handler to execute.
+             * @param {boolean} [is_silent] - if `true` just triggers the handler. Otherwise sets the given `route`
+             * in the browser's addressbar.
+             * @returns {uijet}
+             * @see {@link http://backbonejs.org/docs/backbone.html#section-195|Backbone.History#loadUrl}
+             */
             runRoute        : function (route, is_silent) {
-                is_silent ? Router.dispatch('on', route) : Router.setRoute(route);
+                is_silent ? _router.dispatch('on', route) : _router.setRoute(route);
                 return this;
             }
         });
 
-        return Router;
+        return _router;
     };
 }));
