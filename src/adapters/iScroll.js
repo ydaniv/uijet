fdf2f50b67522f7277c080a9deda0c5c310106c0
Yehonatan Daniv <maggotfish@gmail.com>
2014-07-13 22:08:51 +0300

    Cleaned comments

--- src/adapters/iScroll.js
+++ src/adapters/iScroll.js
@@ -12,7 +12,7 @@
      * iScroll adapter class for the Scrolled mixin.
      * 
      * @class iScroll
-     * * @category Adapter
+     * @category Adapter
      * @extends module:Scrolled
      * @see {@link http://iscrolljs.com/}
      */
