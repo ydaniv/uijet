05391c151150475e6f035450857fb38f71d2a816
Yehonatan Daniv <maggotfish@gmail.com>
2014-07-13 20:19:47 +0300

    Removed unneeded buttons init code that is now done by new initContained() method

--- src/composites/Dialog.js
+++ src/composites/Dialog.js
@@ -3,37 +3,18 @@
         define([
             'uijet_dir/uijet',
             'uijet_dir/widgets/Base',
-            'uijet_dir/widgets/Button',
             'uijet_dir/mixins/Floated'
         ], function (uijet) {
             return factory(uijet);
         });
-    } else {
+    }
+    else {
         factory(uijet);
     }
 }(function (uijet) {
     uijet.Widget('Dialog', {
-        options : {
+        options: {
             type_class: ['uijet_pane', 'uijet_dialog']
-        },
-        register: function () {
-            var buttons_configs,
-                conf,
-                buttons;
-            this._super();
-            // if we have buttons to create
-            if ( buttons_configs = uijet.utils.toArray(this.options.buttons) ) {
-                buttons = [];
-                while ( conf = buttons_configs.shift() ) {
-                    conf.container || (conf.container = this.id);
-                    buttons.push({
-                        type    : 'Button',
-                        config  : conf
-                    });
-                }
-                uijet.start(buttons);
-            }
-            return this;
         }
     }, ['Floated']);
 }));
