3418581180c2f707b2f802dd98bde68a31651b0d
Yehonatan Daniv <maggotfish@gmail.com>
2014-08-10 00:00:06 +0300

    Updated Checkbox with latest changes to API and added docs

--- src/composites/Checkbox.js
+++ src/composites/Checkbox.js
@@ -11,24 +11,32 @@
     }
 }(function (uijet) {
 
+    /**
+     * Checkbox composite class.
+     *
+     * @class Checkbox
+     * @extends Button
+     * @category Composite
+     */
     uijet.Widget('Checkbox', {
         options         : {
             type_class  : ['uijet_button', 'uijet_checkbox']
         },
-        //TODO: add docs
-        check           : function (state) {
-            this.checked = typeof state == 'boolean' ? state : ! this.checked;
-            this.$checkbox[0].checked = this.checked;
-            this.$element.toggleClass('checked', this.checked);
-            return this;
-        },
-        //TODO: add docs
-        disable         : function (state) {
-            this.disabled = typeof state == 'boolean' ? state : ! this.disabled;
-            this.$checkbox[0].disabled = this.disabled;
-            this.$element.toggleClass('disabled', this.disabled);
-            return this;
-        },
+        /**
+         * Creates the shadow DOM checkbox `<input>` element.
+         *
+         * #### Related options:
+         *
+         * * `checkbox`: configuration object for setting up the `<input>` element.
+         *   * `checked`: if `true` the `<input>` will be initialized as checked.
+         *   * `disabled`: if `true` the `<input>` will be initialized as disabled.
+         *   * `value`: sets the initial `<input>`'s value.
+         *   * `name`: sets the `<input>`'s `name` attribute for serialization.
+         *   * `attributes`: an object for setting all other attirbutes of the `<input>` element.
+         *
+         * @methodOf Checkbox
+         * @returns {Checkbox}
+         */
         prepareElement  : function () {
             var ops = this.options.checkbox || {},
                 input_config = uijet.utils.extend({
@@ -49,23 +57,75 @@
 
             checked && this.check(true);
             disabled && this.disable(true);
+
             return this;
         },
-        click           : function (e) {
-            var _publish = this.notify('pre_click', e, this.checked);
+        /**
+         * Sets the instance's checked property and also the `<input>`'s
+         * `checked` property to the given `state` argument if it's a boolean.
+         * Otherwise toggles it.
+         *
+         * @methodOf Checkbox
+         * @param {boolean} [state] - the new checked state to set.
+         * @returns {Checkbox}
+         */
+        check           : function (state) {
+            this.checked = typeof state == 'boolean' ? state : ! this.checked;
+            this.$checkbox[0].checked = this.checked;
+            this.$element.toggleClass('checked', this.checked);
+            return this;
+        },
+        /**
+         * Sets the instance's disabled property and also the `<input>`'s
+         * `disabled` property to the given `state` argument if it's a boolean.
+         * Otherwise toggles it.
+         *
+         * @methodOf Checkbox
+         * @param {boolean} [state] - the new disabled state to set.
+         * @returns {Checkbox}
+         */
+        disable         : function (state) {
+            this.disabled = typeof state == 'boolean' ? state : ! this.disabled;
+            this.$checkbox[0].disabled = this.disabled;
+            this.$element.toggleClass('disabled', this.disabled);
+            return this;
+        },
+        /**
+         * Toggles the `checked` state of the instance and propagates the click
+         * event.
+         *
+         * #### Signals:
+         *
+         * * `pre_click`: triggered at the beginning, passing the click event object and the current
+         * `checked` state as arguments. If `false` is returned the call to {@see Checkbox.check}
+         * and events publishing are skipped.
+         *
+         * #### App events:
+         *
+         * * `<this.id>.clicked`: passes its context under `context`, its new `checked` state and the
+         * `event` object in the data argument.
+         * * `app.clicked`: notifies the rest of the app that a click occurred, passing the click
+         * event object as argument.
+         *
+         * @methodOf Checkbox
+         * @param {Object} event - the click event object.
+         * @returns {Checkbox}
+         */
+        click           : function (event) {
+            var _publish = this.notify('pre_click', event, this.checked);
             if ( _publish !== false ) {
                 this.check()
                     .publish('clicked', {
                         context : this.getContext(),
                         checked : this.checked,
-                        event   : e
+                        event: event
                     });
-                uijet.publish('app.clicked', e);
+                uijet.publish('app.clicked', event);
             }
-            if ( e.target === this.$element[0] && e.eventPhase === 2 ) {
-                e.preventDefault();
+            if ( event.target === this.$element[0] && event.eventPhase === 2 ) {
+                event.preventDefault();
             }
-            e.stopPropagation();
+            event.stopPropagation();
             return this;
         }
     }, {
