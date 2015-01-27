b591f05c109b756d179c017735df77f4e8aa5309
ydaniv <maggotfish@gmail.com>
2015-01-27 10:54:33 +0200

    Fixed flow in bindData()

--- src/modules/binding/rivets-backbone.js
+++ src/modules/binding/rivets-backbone.js
@@ -139,23 +139,18 @@
                 k, observable;
 
             // if observe option is not set but we have a resource option set
-            if ( ! observable ) {
-
-                observables = {};
-
-                // resource option is set
-                if ( resource ) {
-                    // if resource is a name in resources registry
-                    if ( typeof resource == 'string' ) {
-                        // add it to observables under its name
-                        observables[resource] = this.resource || uijet.Resource(resource);
-                    }
-                    else {
-                        this.bindDataEvents();
-                        // implicitly skip
-                        // we're probably handling observation in a higher level, i.e. "each-" binding
-                        return this;
-                    }
+            if ( ! observables && resource ) {
+                // if resource is a name in resources registry
+                if ( typeof resource == 'string' ) {
+                    observables = {};
+                    // add it to observables under its name
+                    observables[resource] = this.resource || uijet.Resource(resource);
+                }
+                else {
+                    this.bindDataEvents();
+                    // implicitly skip
+                    // we're probably handling observation in a higher level, i.e. "each-" binding
+                    return this;
                 }
             }
 
