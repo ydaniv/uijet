d55f129f447819c4171bbfb71ce83603faf1bc1a
ydaniv <maggotfish@gmail.com>
2015-05-14 11:15:37 +0300

    Adapt jqScroll adapters to updates in the library

--- src/adapters/jqWheelScroll.js
+++ src/adapters/jqWheelScroll.js
@@ -1,12 +1,13 @@
-(function (factory) {
+(function (root, factory) {
     if ( typeof define === 'function' && define.amd ) {
         define(['uijet_dir/uijet', 'jquery.mousewheel', 'jqscroll'], function (uijet) {
-            return factory(uijet);
+            return factory(root, uijet);
         });
     } else {
-        factory(uijet);
+        factory(root, root.uijet);
     }
-}(function (uijet) {
+}(this, function (root, uijet) {
+    'use strict';
 
     /**
      * jquery-mousewheel + jqScroll adapter class for the Scrolled mixin.
@@ -33,22 +34,30 @@
          */
         scroll  : function () {
             var that = this, el, is_horizontal,
-                jqS_ops = this.options.jqscroll_options || {};
+                jqS_ops = this.options.jqscroll_options || {},
+                scroller;
 
             if ( is_horizontal = (this.options.horizontal && ! this.options.grid_layout) ) {
                 jqS_ops.horizontal = true;
                 jqS_ops.vertical = false;
             }
-            this._wrap().$wrapper.scroller(jqS_ops);
-            el = this.$element[0];
-            this.$element.unmousewheel().mousewheel(function (e, delta, dx, dy) {
-                // prevent the default scrolling and keep it inside the widget
-                e.preventDefault();
-                //TODO: consider for optimization using cache based offset instead of using offsetLeft/Top which cause reflow
-                that.scrollTo(is_horizontal ? (-el.offsetLeft - 37*dx) : (-el.offsetTop - 37*dy));
-            });
 
-            this.scroll_on = true;
+            scroller = this._wrap().$wrapper.scroller(jqS_ops).data('scroller');
+
+            if ( scroller ) {
+                el = this.$element[0];
+                this.$element.unmousewheel().mousewheel(function (e, delta, dx, dy) {
+                    // prevent the default scrolling and keep it inside the widget
+                    e.preventDefault();
+                    //TODO: consider for optimization using cache based offset instead of using offsetLeft/Top which cause reflow
+                    that.scrollTo(is_horizontal ? (- el.offsetLeft - 37 * dx) : (- el.offsetTop - 37 * dy));
+                });
+
+                this.scroll_on = true;
+            }
+            else {
+                this.scroll_on = false;
+            }
 
             return this;
         },
